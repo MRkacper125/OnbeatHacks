b8836edc755c56aba8e1862d4843bd2c6435cff0
MRkacper125 <kacper.kleszowski@gmail.com>
2026-09-09 00:30:31 +0200

    speed fix4

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function drawMatrix() {
 setupMatrix();
 
 // większa liczba = wolniejsza animacja
-setInterval(drawMatrix, 100);
+setInterval(drawMatrix, 250);
 
 window.addEventListener(
     "resize",
